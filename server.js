5a164445fb6bc2aa1d3d7c3f89924322d0391a3d
Chikoom <callforcopy@gmail.com>
2020-07-20 12:22:05 +0300

    Router working

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
+const router = require('./server/routes/api')
 const app = express()
+
 
 mongoose.connect("mongodb://localhost/weather",
 { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/', router)
 
 const PORT = 4321
 app.listen(PORT, function(){
