63dda7ceba934d610f38007e437f4b23329cbb81
Chikoom <callforcopy@gmail.com>
2020-07-20 15:38:45 +0300

    POST, DELETE all routes created

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const axios = require('axios')
+const { Mongoose } = require('mongoose')
+const City = require('../model/City')
 const router = express.Router()
 const API_KEY = process.env.WEATHER_API_KEY || ''
 const API_URL = process.env.WEATHER_URL || ''
@@ -14,8 +16,53 @@ router.get('/city/:cityName', async (req,res) => {
   try{
     let response = await axios.get(API_URL,{params:{appid: API_KEY,q:cityName}})
     res.send(response.data)
-  }catch(e){
-    console.error(e)
+  }catch(err){
+    console.error(err)
+    res.status(500)
+  }finally{
+    res.end()
+  }
+})
+
+router.post('/city', async (req, res) => {
+  //validateData(req.body)
+  try {
+    const cityToSave = new City({
+      name: "AMSTERDAM",
+      temp: 999,
+      condition: "Sunny",
+      conditionPic:"google.com"
+    })
+    const response = await cityToSave.save()
+    res.send(response)
+  }catch(err){
+    console.error(err)
+    res.status(500)
+  }finally{
+    res.end()
+  }
+})
+
+router.get('/cities', async (req,res) => {
+  try{
+    let response = await City.find({}).exec()
+    res.send(response)
+  }catch(err){
+    console.error(err)
+    res.status(500)
+  }finally{
+    res.end()
+  }
+})
+
+router.delete('/city/:cityId', async (req,res) => {
+  const {cityId} = req.params
+  try{
+    let response = await City.findOneAndDelete({_id:cityId}).exec()
+    res.send(response)
+  }catch(err){
+    console.error(err)
+    res.status(500)
   }finally{
     res.end()
   }
