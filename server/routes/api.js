ef994927a52713d8655e018973699f2ed02f30dd
Chikoom <callforcopy@gmail.com>
2020-07-24 21:33:46 +0300

    creates helper functions

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const axios = require('axios')
-const mongoose = require('mongoose')
 const City = require('../model/City')
 const router = express.Router()
 const API_KEY = process.env.WEATHER_API_KEY || ''
@@ -13,165 +12,170 @@ router.get('/sanity', (req,res) => {
 })
 
 router.get('/city/:cityName', async (req,res) => {
-  console.log(req.params.cityName)
   const {cityName} = req.params
-  try{
-    let response = await axios.get(`${API_URL}/weather`,{params:{units:'metric',appid: API_KEY,q:cityName}})
+  try {
+    let cityWeather = await getCityWeatherByName(cityName)                                                           
     let cityImage = await getCityImage(cityName)
-    console.log(cityImage)
-    const formatedRes = {
-      cityId:response.data.id,
-      temp: response.data.main.temp,
-      feelsLike: response.data.main.feels_like,
-      name: response.data.name,
-      icon: response.data.weather[0].icon,
-      condition: response.data.weather[0].description,
-      lastUpdate: new Date(),
-      cityImage: cityImage
-    }
-    res.send(formatedRes)
-  }catch(err){
+    let formatedObject = formatObject(cityWeather, cityImage)
+    res.send(formatedObject)
+  } catch(err) {
     console.error(err)
     res.status(500)
-  }finally{
-    res.end()
+    res.send({error: "Error recieving city data"})
   }
 })
 
 router.get('/city/:lat/:lon', async (req,res) => {
-  console.log(req.params)
   const {lat, lon} = req.params
-  try{
-    let response = await axios.get(`${API_URL}/weather`,{params:{units:'metric',appid: API_KEY,lat,lon}})
-    let cityImage = await getCityImage(response.data.name)
-    console.log(response)
-    const formatedRes = {
-      cityId:response.data.id,
-      temp: response.data.main.temp,
-      feelsLike: response.data.main.feels_like,
-      name: response.data.name,
-      icon: response.data.weather[0].icon,
-      condition: response.data.weather[0].description,
-      lastUpdate: new Date(),
-      cityImage: cityImage
-    }
-    res.send(formatedRes)
-  }catch(err){
+  try {
+    let cityWeather = await getCityWeatherByLatLon(lat, lon) 
+    let cityImage = await getCityImage(cityWeather.name)
+    let formatedObject = formatObject(cityWeather,cityImage)
+    res.send(formatedObject)
+  } catch(err) {
     console.error(err)
     res.status(500)
-  }finally{
-    res.end()
+    res.send({error: "Error recieving city by location data"})
   }
 })
 
 router.post('/city', async (req, res) => {
-  const {name,temp,condition,feelsLike,icon,cityId,lastUpdate,cityImage} = req.body
-  //validateData(req.body)
+  const {name, temp, condition, feelsLike, icon, cityId, lastUpdate, cityImage} = req.body
   try {
-    const cityToSave = new City({name,temp,feelsLike,condition,icon,cityId,lastUpdate,cityImage})
+    const cityToSave = new City({name, temp, condition, feelsLike, icon, cityId, lastUpdate, cityImage})
     const response = await cityToSave.save()
     res.send(response)
-  }catch(err){
+  } catch(err) {
     console.error(err)
     res.status(500)
-  }finally{
-    res.end()
+    res.send({error: "Error Saving City"})
   }
 })
 
 router.get('/cities', async (req,res) => {
-  try{
+  try {
     let response = await City.find({}).exec()
     res.send(response)
-  }catch(err){
+  } catch(err) {
     console.error(err)
     res.status(500)
-  }finally{
-    res.end()
+    res.send({error: "Error Getting cities from DB"})
   }
 })
 
 router.delete('/city/:cityId', async (req,res) => {
   const {cityId} = req.params
-  console.log(cityId)
-  try{
+  try {
     let response = await City.findOneAndDelete({_id:cityId}).exec()
     res.send(response)
-  }catch(err){
-    //console.error(err)
+  } catch(err) {
+    console.error(err)
     res.status(500)
-  }finally{
-    res.end()
+    res.send({error: "Error deleting city from DB"})
   }
 })
 
 router.put('/updateCity', async (req, res) => {
   const allCities = await City.find({}).exec()
   let citiesIds = allCities.map(city => city.cityId).join(',')
-  try{
-    let response = await axios.get(`${API_URL}/group`,{params:{id:citiesIds,units:'metric',appid: API_KEY}})
-    const promiseArray = []
-    response.data.list.forEach(city => {
-      const formatedRes = {
-        cityId:city.id,
-        temp: city.main.temp,
-        feelsLike: city.main.feels_like,
-        name: city.name,
-        icon: city.weather[0].icon,
-        condition: city.weather[0].description,
-        lastUpdate: new Date()
-      }
-      //console.log(formatedRes)
-      promiseArray.push(City.findOneAndUpdate({cityId:city.id},{$set:formatedRes},{useFindAndModify:false,new: true}).exec())
-    })
-    Promise.all(promiseArray).then(data => {
-      console.log(data)
-      res.send(data)
-    })
-    
-  }catch (err) {
-    console.log(err)
-    res.end()
-  }
-  
+  getCitiesByIds(citiesIds)
+  .then(data => res.send(data))
+  .catch(err => {
+    console.error(err)
+    res.status(500)
+    res.send({error: "Error updating cities"})
+  })
 })
 
 router.put('/updateCity/:cityId', async (req, res) => {
   const cityToUpdate = await City.find({_id:req.params.cityId}).exec()
-  
-  try{
-    let response = await axios.get(`${API_URL}/weather`,{params:{id:cityToUpdate[0].cityId,units:'metric',appid: API_KEY}})
-    console.log(response)
-    const formatedRes = {
-      cityId:response.data.id,
-      temp: response.data.main.temp,
-      feelsLike: response.data.main.feels_like,
-      name: response.data.name,
-      icon: response.data.weather[0].icon,
-      condition: response.data.weather[0].description,
-      lastUpdate: new Date()
-    }
-    const savedItem = await City.findOneAndUpdate({cityId:formatedRes.cityId},{$set:formatedRes},{useFindAndModify:false,new: true}).exec()
-
-    //console.log(savedItem)
+  try {
+    let response = await getCityWeatherById(cityToUpdate[0].cityId)
+    const formatedRes = formatObject(response)
+    const savedItem = await City.findOneAndUpdate({cityId:formatedRes.cityId},
+                                                  {$set:formatedRes},
+                                                  {useFindAndModify:false,new: true})
+                                                  .exec()
     res.send(savedItem)
-    res.end()
-
-  }catch (err) {
-    console.log(err)
-    res.end()
+  } catch (err) {
+    console.error(err)
+    res.status(500)
+    res.send({error: "Error updating city"})
   }
-  
 })
+
+const getCityWeatherById = async (cityId) => {
+  let response = await axios.get(`${API_URL}/weather`,{params:{
+    units:'metric',
+    appid: API_KEY,
+    id:cityId
+  }})
+  return response.data
+}
+
+const getCitiesByIds = async (idsArray) => {
+  let response = await axios.get(`${API_URL}/group`, {params:{
+    id:idsArray,
+    units:'metric',
+    appid: API_KEY
+  }})
+  const promiseArray = []
+  response.data.list.forEach(city => {
+  const formatedRes = formatObject(city)
+  promiseArray.push(
+  City.findOneAndUpdate(
+  {cityId:city.id},
+  {$set:formatedRes},
+  {useFindAndModify:false, new: true})
+  .exec())})
+  return Promise.all(promiseArray)
+}
+
+const getCityWeatherByName = async (cityName) => {
+  let response = await axios.get(`${API_URL}/weather`,{params:{
+    units:'metric',
+    appid: API_KEY,
+    q:cityName
+  }})
+  return response.data
+}
+
+const getCityWeatherByLatLon = async (lat, lon) => {
+  let response = await axios.get(`${API_URL}/weather`, {params:{
+    units:'metric',
+    appid: API_KEY,
+    lat,lon
+  }})
+  return response.data
+}
 
 const getCityImage = async (cityName) => {
   try{
-    let response = await axios.get(`${IMAGE_URL}`,{params:{per_page:1,page:1,client_id:IMAGE_KEY,query:cityName}})
-    return response.data.results[0].urls.full
+    let response = await axios.get(`${IMAGE_URL}`,{params:{
+                                                    per_page:1,
+                                                    page:1,
+                                                    client_id:IMAGE_KEY,
+                                                    query:cityName
+                                                  }})
+    const imageURL = response.data.results[0].urls.regular
+    return imageURL
   } catch (err) {
-
+    return ''
   }
-  
+}
+
+const formatObject = (data, cityImage = null) => {
+  const cityObjectToReturn = {
+    cityId:data.id,
+    temp: data.main.temp,
+    feelsLike: data.main.feels_like,
+    name: data.name,
+    icon: data.weather[0].icon,
+    condition: data.weather[0].description,
+    lastUpdate: new Date()
+  }
+  if (cityImage) cityObjectToReturn.cityImage = cityImage
+  return cityObjectToReturn
 }
 
 module.exports = router
